966594cacd5a4759e3c02452dfa643370ab6f2d4
rizkyizh <rizkiizzulhaq14@gmail.com>
2023-06-26 08:36:34 +0700

    change  description course typedata db to TEXT

--- src/main/java/com/registration/course/serverapp/api/course/Course.java
+++ src/main/java/com/registration/course/serverapp/api/course/Course.java
@@ -9,6 +9,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
@@ -43,6 +44,7 @@ public class Course {
   private String instructor;
 
   @NotBlank(message = "description tidak boleh kosong")
+  @Lob
   private String description;
 
   @NotBlank(message = "price tidak boleh kosong")
