65fc6ad0f91dbf86f098a2f80739e74c1a1fa91a
rizkyizh <rizkiizzulhaq14@gmail.com>
2023-06-27 11:44:32 +0700

    fix history controller

--- src/main/java/com/registration/course/serverapp/api/transaction/history/HistoryController.java
+++ src/main/java/com/registration/course/serverapp/api/transaction/history/HistoryController.java
@@ -2,12 +2,16 @@ package com.registration.course.serverapp.api.transaction.history;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.registration.course.serverapp.api.authentication.AppUserDetail;
 import com.registration.course.serverapp.api.dto.response.ResponseData;
+import com.registration.course.serverapp.api.user.User;
+import com.registration.course.serverapp.api.user.UserService;
 
 import lombok.AllArgsConstructor;
 
@@ -19,6 +23,9 @@ public class HistoryController {
   @Autowired
   private HistoryService historyService;
 
+  @Autowired
+  private UserService userService;
+
   @GetMapping
   public ResponseEntity<ResponseData<History>> getAllTransactions() {
     ResponseData<History> responseData = new ResponseData<>();
@@ -28,12 +35,24 @@ public class HistoryController {
     return ResponseEntity.ok(responseData);
   }
 
-  @GetMapping("/member/{id}")
-  public ResponseEntity<ResponseData<History>> getAllHistoriesByMemberId(@PathVariable Integer id) {
+  @GetMapping("/member")
+  public ResponseEntity<ResponseData<History>> getAllHistoriesByMemberId() {
     ResponseData<History> responseData = new ResponseData<>();
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+    if (authentication != null) {
+      Object principal = authentication.getPrincipal();
+
+      if (principal instanceof AppUserDetail) {
+        AppUserDetail appUserDetail = (AppUserDetail) principal;
+        String username = appUserDetail.getUsername();
+        // Melakukan pemrosesan lebih lanjut dengan informasi pengguna
+        User user = userService.getByUsername(username);
+        responseData.setPayload(historyService.getAllHistoriesByMemberId(user.getId()));
+      }
+    }
     responseData.setStatus(true);
     responseData.getMessages().add("berhasil ditemukan");
-    responseData.setPayload(historyService.getAllHistoriesByMemberId(id));
     return ResponseEntity.ok(responseData);
   }
 
